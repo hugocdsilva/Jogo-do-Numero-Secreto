7fef50627e178a576cdb639a4271e869b7fdd229
Hugo Silva <hugocorreia1997@gmail.com>
2024-01-19 17:51:26 -0300

    informando o console

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if(chute == numeroSecreto) {
+    console.log('Você acertou o número secreto (29)');
+}
