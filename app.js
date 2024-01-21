b0acb0e995bd98a952c8ee1ae1275a6eab979266
Hugo Silva <hugocorreia1997@gmail.com>
2024-01-20 22:06:40 -0300

    upload 1

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
+let numeroSecreto = 11;
+
+console.log('Número secreto',numeroSecreto);//Esse comando só é possível ver no console dentro browers
 let chute = prompt('Escolha um número entre 1 e 30');
 
+// Se o chute for igual ao número secreto 
 if(chute == numeroSecreto) {
-    console.log('Você acertou o número secreto (29)');
-}
+    alert('Você acertou o número secreto ('+numeroSecreto+')');
+}else{alert('Você errou!')}
